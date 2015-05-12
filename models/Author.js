3b81d3382404763721a68249136a1cd9b6d53f90
Marco Sportillo <msportillo@gmail.com>
2015-05-12 23:04:36 +0200

    author and book model (to fix)

---
+++ models/Author.js
@@ -0,0 +1,27 @@
+var keystone = require('keystone'),
+	Types = keystone.Field.Types;
+
+/**
+ * Author Model
+ * ==========
+ */
+
+var Author = new keystone.List('Author');
+
+Author.add({
+	
+	name: { type: Types.Text, initial: true, required: true, index: true },
+	surname: { type: Types.Text, initial: true, required: true, index: true }
+});
+
+
+/**
+ * Registration
+ */
+
+function getAbbreviation() {
+	return this.surname + ", " + this.name[0] + ".";
+}
+
+Author.defaultColumns = 'id, name, surname';
+Author.register();
---
+++ models/Book.js
@@ -0,0 +1,25 @@
+var keystone = require('keystone'),
+	Types = keystone.Field.Types;
+
+/**
+ * Book Model
+ * ==========
+ */
+
+var Book = new keystone.List('Book');
+
+Book.add({
+	isbn: { type: Types.Text, initial: true, required: true, index: true },
+	title: { type: Types.Text, initial: true, required: true, index: true },
+	authors: { type: Types.Relationship, ref: 'Author' },
+	numCopies: {type: Types.Number, initial: true}
+}, 'Available', {
+	isAvailable: { type: Boolean, label: 'Available'}
+});
+
+/**
+ * Registration
+ */
+
+Book.defaultColumns = 'isbn, title, author, isAvailable';
+Book.register();
